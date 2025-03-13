48fa0af370e76c31aff18695c4131c933cbd6c07
mmilushev91 <milushev_1990@abv.bg>
2025-03-13 14:40:42 +0200

    added exercise

---
+++ javaScript/101/script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// JavaScript Syntax
+var name = "Marian";
+
+if (name === "Marian") {
+  alert("Hello Student!");
+}
+
+function greeting(name) {
+  return `Hello ${name}, welcome to JavaScript`;
+}
+
+//Console logging
+
+console.log("Write anything here");
+console.warn("This is a warning");
+console.error("This is an error");

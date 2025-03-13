f9c131f79dc3c0ad2741543d6fdb02377e7d63b3
mmilushev91 <milushev_1990@abv.bg>
2025-03-13 14:59:18 +0200

    added exercise

--- javaScript/101/script.js
+++ javaScript/101/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-// JavaScript Syntax
+// 1. JavaScript Syntax
 var name = "Marian";
 
 if (name === "Marian") {
@@ -11,8 +11,37 @@ function greeting(name) {
   return `Hello ${name}, welcome to JavaScript`;
 }
 
-//Console logging
+console.log("---------------------------------");
+
+//2. Console logging
 
 console.log("Write anything here");
 console.warn("This is a warning");
 console.error("This is an error");
+
+console.log("---------------------------------");
+
+//3. Debugging external files
+
+console.log("Page successfully loaded script.js");
+
+console.log("---------------------------------");
+
+//4. Storing data in variables
+
+var website = "https://codingforeverybody.com/";
+var teacherName = "Kalob Taulien";
+
+console.log(teacherName);
+
+console.log("---------------------------------");
+
+//5. Let const variable
+
+let apples = 1;
+
+// const and let are scoped to the {} in which they were created
+if (teacherName === "Kalob Taulien") {
+  let apples = 5;
+  const height = "6'0";
+}
